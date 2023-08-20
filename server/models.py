373a61659d912d9fb3e74d2d156369c102f1f349
+ <+>
2023-08-20 16:24:05 -0400

    added User and post models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,28 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-
+from datetime import datetime
 from config import db
 
-# Models go here!
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    lastName = db.Column(db.String)
+    countryOfBirth = db.Column(db.String)
+
+    posts = db.relationship('Post', backref = 'users')
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    content = db.Column(db.String)
+    createdAt = db.Column(db.DateTime, default=datetime.utcnow)
+    views = db.Column(db.Integer, default=0)
+
+
+
+
