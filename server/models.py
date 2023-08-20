f6486a76f7abf85fc7d14e8e96c357952a63421d
+ <+>
2023-08-20 16:39:09 -0400

    created repr functions and completed relationship between a post and a user

--- server/models.py
+++ server/models.py
@@ -2,17 +2,20 @@ from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from datetime import datetime
 from config import db
+from ipdb import set_trace
 
 class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
-    lastName = db.Column(db.String)
+    username = db.Column(db.String, nullable = False)
+    email = db.Column(db.String, unique=True, nullable = False)
     countryOfBirth = db.Column(db.String)
 
-    posts = db.relationship('Post', backref = 'users')
+    posts = db.relationship('Post', backref = 'user')
+
+    def __repr__(self):
+        return f'{self.username}'
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -22,6 +25,13 @@ class Post(db.Model):
     content = db.Column(db.String)
     createdAt = db.Column(db.DateTime, default=datetime.utcnow)
     views = db.Column(db.Integer, default=0)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
+
+    def __repr__(self):
+        return f'"{self.title}" was created at {self.createdAt} by {self.user_id}'
+
+set_trace()
 
 
 
