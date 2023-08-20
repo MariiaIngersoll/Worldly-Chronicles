03a6d0ba50811a77baab24ca208d7310fa9704da
+ <+>
2023-08-20 16:08:30 -0400

    created database

---
+++ server/migrations/versions/4383015e8924_create_database.py
@@ -0,0 +1,24 @@
+"""Create database
+
+Revision ID: 4383015e8924
+Revises: 
+Create Date: 2023-08-20 15:12:07.307565
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4383015e8924'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
