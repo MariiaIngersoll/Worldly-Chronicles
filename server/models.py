0791979a0c16899e18c3d639c1a80bc29e326779
+ <+>
2023-08-21 20:43:56 -0400

    added join table for Post and Location models to create many to many relationship

--- server/models.py
+++ server/models.py
@@ -4,6 +4,12 @@ from datetime import datetime
 from config import db
 from ipdb import set_trace
 
+
+post_location_association = db.Table('post_location_association',
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+    db.Column('location_id', db.Integer, db.ForeignKey('locations.id'))
+)
+
 class User(db.Model):
     __tablename__ = 'users'
 
@@ -23,12 +29,15 @@ class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    image = db.Column(db.String)
     content = db.Column(db.String)
     createdAt = db.Column(db.DateTime, default=datetime.utcnow)
     views = db.Column(db.Integer, default=0)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
     comments = db.relationship('Comment', backref = 'post')
+
+    locations = db.relationship('Location', secondary=post_location_association, back_populates='posts')
 
     def __repr__(self):
         return f'"{self.title}" was created at {self.createdAt} by {self.user_id}'
@@ -49,6 +58,8 @@ class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     country = db.Column(db.String)
     city = db.Column(db.String)
+
+    posts = db.relationship('Post', secondary=post_location_association, back_populates='locations')
 
 
 
