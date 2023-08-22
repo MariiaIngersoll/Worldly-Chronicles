cec9cc51bb13cfdeb685cd25d62e656ebd4c613d
+ <+>
2023-08-21 20:29:03 -0400

    populated User model with fake data

--- server/seed.py
+++ server/seed.py
@@ -6,12 +6,28 @@ from random import randint, choice as rc
 # Remote library imports
 from faker import Faker
 
+fake = Faker()
 # Local imports
 from app import app
-from models import db
+from models import User, Post, Comment, Location, db
+
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        users = []
+
+        for i in range(200):
+            user = User(
+                username = fake.user_name(),
+                email = fake.email(),
+                countryOfBirth = fake.country()
+            )
+            db.session.add(user)
+            db.session.commit()
+
+
+
+
