442a0e9d9940ff5c691b8c396c0749227c4a3dba
+ <+>
2023-08-24 00:29:25 -0400

    added delete method for post route

--- server/routes/post.py
+++ server/routes/post.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from config import api , db
 from models import Post
-from flask import request
+from flask import request, make_response
 
 class PostsResource(Resource):
     def get(self):
@@ -12,14 +12,27 @@ class PostsResource(Resource):
         data = request.get_json()
         title = data.get('title')
         content = data.get('content')
-        post = Post(
+        new_post = Post(
             title = title,
             content = content
         )
+        db.session.add(new_post)
+        db.session.commit()
+        response = make_response(
+            new_post.to_dict(),
+            201
+        )
+        return response
+    
+    def delete(self, post_id):
+        post = Post.query.filter_by(id=post_id).first()
+        if post: 
+            db.session.delete(post)
+            db.session.commit()
+            return "", 204
+        else:
+            return {"message": "Post not found!"}, 404
 
-        return post, 201
-    
-    
     
 api.add_resource(PostsResource, "/posts")
 
