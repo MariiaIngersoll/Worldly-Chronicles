a105930efe9fce2b08520c1dbab9b84d77bbc2af
+ <+>
2023-08-25 19:47:58 -0400

    rendered pictures and location for each post

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -1,13 +1,35 @@
-import React, {useState} from "react"
+import React from "react";
 
-function SinglePost({post}){
-    return (
-        <li className="card">
-        <div className="image">   
-            <h2>{post.title}</h2>
-        </div>
-    </li>
-    )
+function SinglePost({ post }) {
+  console.log("Post:", post); // Add this line to check the data received for the post
+
+  return (
+    <div className="card">
+      <div className="image">
+        <h2>{post.title}</h2>
+        <ul>
+          {post.images.map((image) => (
+            <li key={image.id}>
+              <img className = "PostImg" src={image.url} alt={post.title} />
+            </li>
+          ))}
+        </ul>
+        <ul>
+        {post.locations.map((loc) => (
+            <div key={loc.id}>
+                <h2>{loc.country}, {loc.city}</h2>
+        
+            </div>
+            ))}
+        </ul>
+        <p>{post.content}</p>
+      </div>
+    </div>
+  );
 }
 
-export default SinglePost
+export default SinglePost;
+
+
+
+
