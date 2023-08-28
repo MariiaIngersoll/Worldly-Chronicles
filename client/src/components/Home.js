f5a4d846741466470b6b3fb767ed59da98eab46b
+ <+>
2023-08-27 23:42:50 -0400

    styled homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,9 +6,7 @@ function Home() {
   return (
     <div className="home-container">
       <h1 className="home-title">Welcome to Worldly Chronicles!</h1>
-      <p className="home-description">
-        Discover exciting travel destinations and explore amazing posts.
-      </p>
+      <p className="home-paragraph"> Join us on a journey through captivating narratives that bring the world's most stunning places to life. Our blog lets you explore iconic landmarks, tranquil escapes, and cultural treasures without leaving your seat.</p>
       <div className="home-links">
         <Link to="/posts" className="home-link">
           Explore All Posts
